f42cf1de70e579bfa50bf01bf1049b1026aff2b9
vortexso <66820371+ShergoMani@users.noreply.github.com>
2025-11-27 21:59:53 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -9,3 +9,11 @@ self.addEventListener('activate', (e) => {
 self.addEventListener('fetch', (e) => {
   e.respondWith(fetch(e.request));
 });
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open("app-cache").then(cache => {
+      return cache.addAll(["/", "/index.html", "/manifest.json"]);
+    })
+  );
+});
